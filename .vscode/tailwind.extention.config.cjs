812b631fb1b67a77c9e2f8046f027a6b042526d8
ccpu <m.doaie@hotmail.com>
2025-12-26 13:05:51 +0000

    feat(tailwind): add Tailwind CSS configuration file
    * Introduced a new `tailwind.extention.config.cjs` file for Tailwind CSS setup.
    * Configures content paths for scanning files in `apps` and `packages` directories.
    * Sets up an initial theme and plugin structure for future customization.

---
+++ .vscode/tailwind.extention.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../apps/**/*.{js,jsx,ts,tsx,mdx,html}',
+    '../packages/**/*.{js,jsx,ts,tsx,mdx,html,css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
