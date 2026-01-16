b61dd1c4c88af9b859ec2e57b154d25ed88a164e
ccpu <m.doaie@hotmail.com>
2026-01-16 18:48:04 +0000

    feat(theme-toggle): remove ThemeToggle component and related stories
    * Deleted `ThemeToggle` component and its associated story files.
    * Updated exports in `index.ts` to reflect the removal.

--- packages/shadcn-ui/src/theme-toggle/index.ts
+++ packages/shadcn-ui/src/theme-toggle/index.ts
@@ -2,4 +2,3 @@ export * from './ThemeModeDropdown';
 export * from './ThemeModeSwitchInside';
 export * from './ThemeModeSwitchOutside';
 export * from './ThemeModeToggleButton';
-export * from './ThemeToggle';
